15074879e1159ac95dc55cfb9758744de54813f5
Hao kexin <kashin.k@prime-strategy.co.jp>
2020-03-04 18:03:24 +0900

    change post-branch.php

--- post-branch.php
+++ post-branch.php
@@ -10,36 +10,12 @@ License: GNU General Public License v3.0
 */
 
 
-/*------------------------------------------------------------------------------------
-*** Description ***
-......................................................................................
-This is the post branch function that you can create a branch of a publishing post.
-......................................................................................
-
-*** Instructions ***
-......................................................................................
-You can create a branch by clicking the button above the Move to Trash.
-You can make any changes to the branch.
-Just click the branch's publish/update to integrate the content of the branch into the original post.
-......................................................................................
-
-*** Details ***
-......................................................................................
-Created branches exist independently until they are made public.
-Branch inherits all attributes of the original post.
-Including custom_keys, post_meta, attachment, taxonomies, terms, ACF-fields.
-Revisions from branches will also be inherited from the original post after the publication.
-Branch will be deleted after publish.
-......................................................................................
-
-Created on February 22, 2020, Tokyo By Haokexin
-------------------------------------------------------------------------------------*/
-
 /*
+*** For Classic Editor ***
 Show button when post is public, future, privacy status
 But the button is not showed for branches that have already made future
 */
-function add_post_submitbox_button() {
+function wbfp_add_post_submitbox_button() {
 	global $post;
 	$show_button_which_post_status = array(	'publish', 
 						'future', 
@@ -51,21 +27,21 @@ function add_post_submitbox_button() {
 		}
 	}
 }
-add_action( 'post_submitbox_start', 'add_post_submitbox_button' );
+add_action( 'post_submitbox_start', 'wbfp_add_post_submitbox_button' );
 
 /*
 Create a branch before an existing post is updated in the database
 When $_POST including the [create_branch]
 */
-function create_post_branch( $id ) {
+function wbfp_create_post_branch( $id ) {
 	if ( isset( $_POST['create_branch'] ) ) {
 		$origin = get_post( $id, ARRAY_A );
 		unset( $origin['ID'] );
 		$origin['post_status'] = 'draft';
 		$origin['post_name']   = $origin['post_name'] . '-branch';
 		$branch_id = wp_insert_post( $origin );
-		copy_post_post_meta( $id, $branch_id );
-		copy_post_taxonomies( $id, $branch_id, $origin['post_type'] );
+		wbfp_copy_post_post_meta( $id, $branch_id );
+		wbfp_copy_post_taxonomies( $id, $branch_id, $origin['post_type'] );
 		add_post_meta($branch_id, '_original_post_id', $id);
 		$user = wp_get_current_user();
 		add_post_meta($branch_id, '_creator_name', $user->display_name, true );
@@ -76,37 +52,38 @@ function create_post_branch( $id ) {
 		}
 	}
 }
-add_filter( 'pre_post_update', 'create_post_branch' );
+add_filter( 'pre_post_update', 'wbfp_create_post_branch' );
 	
 /*
 Show info on branch post/pages editor
 */
-function post_branch_admin_notice() {
-	if ( isset( $_REQUEST['post'] ) ) {
-		$branch_id = $_REQUEST['post'];
+function wbfp_post_branch_admin_notice() {
+	global $pagenow;
+	if ( $pagenow == 'post.php' ) {
+		$branch_id = get_post_id();
 		if ( $original_id = get_post_meta( $branch_id, '_original_post_id', true ) ) {
 			$creator_name = get_post_meta( $branch_id, '_creator_name', true );
-			echo '<div class="updated fade" style="text-align:center; color:blue;"><p>' . sprintf( "The post is a branch of <a href='%s' target='__blank' >%s</a>. Branch creator is %s ",  get_permalink($original_id), $original_id, $creator_name ) . '</p></div>';
+			echo '<div class="updated fade" style="text-align:center; color:blue;"><p>' . sprintf( "The post is a branch of <a href='%s' target='__blank' >%s</a>.   Branch creator is %s ",  get_permalink($original_id), $original_id, $creator_name ) . '</p></div>';
 		}
 	}
 }
-add_action( 'admin_notices', 'post_branch_admin_notice' );
+add_action( 'admin_notices', 'wbfp_post_branch_admin_notice' );
 
 /*
 Add hooks for custom post_type
 */
-function add_custom_post_type_update_hooks() {
+function wbfp_add_custom_post_type_update_hooks() {
 	$additional_post_types = get_post_types( array( '_builtin' => false, 'show_ui' => true ) );
 	foreach ( $additional_post_types as $post_type ) {
 		add_action( 'publish_' . $post_type, 'original_post_pages_update', 9999, 2 );
 	}
 }
-add_action( 'init', 'add_custom_post_type_update_hooks', 9999 );
+add_action( 'init', 'wbfp_add_custom_post_type_update_hooks', 9999 );
 
 /*
 Update post/page when branch published
 */
-function original_post_pages_update( $id, $post ) {
+function wbfp_original_post_pages_update( $id, $post ) {
 	if ( $original_id = get_post_meta( $id, '_original_post_id', true ) ) {
 		$post = $post->to_array();
 		$post['ID'] = $original_id;
@@ -115,22 +92,22 @@ function original_post_pages_update( $id, $post ) {
 			$post['post_name'] 
 			);
 		copy_post_post_meta( $id, $original_id );
-		inherited_branch_attachments( $id, $original_id );
-		copy_post_taxonomies( $id, $original_id, $post['post_type'] );
-		inherited_branch_revision( $id, $original_id );
+		wbfp_inherited_branch_attachments( $id, $original_id );
+		wbfp_copy_post_taxonomies( $id, $original_id, $post['post_type'] );
+		wbfp_inherited_branch_revision( $id, $original_id );
 		wp_update_post( $post );
 		wp_delete_post( $id );
 		wp_safe_redirect( admin_url( '/post.php?post=' . $original_id . '&action=edit&message=1' ) );
 		exit;
  	}
 }	
-add_action( 'publish_page', 'original_post_pages_update', 9999, 2 );
-add_action( 'publish_post', 'original_post_pages_update', 9999, 2 );
+add_action( 'publish_page', 'wbfp_original_post_pages_update', 9999, 2 );
+add_action( 'publish_post', 'wbfp_original_post_pages_update', 9999, 2 );
 
 /*
 Show creator's info in the post list
 */
-function show_branch_info_in_list( $info ) {
+function wbfp_show_branch_info_in_list( $info ) {
 	global $post;
 	if ( 	$original_id = get_post_meta( $post->ID, '_original_post_id', true ) ) {
 		$creator_name = get_post_meta( $post->ID, '_creator_name', true );
@@ -139,13 +116,13 @@ function show_branch_info_in_list( $info ) {
 	}
 	return $info;
 }
-add_filter( 'display_post_states', 'show_branch_info_in_list' );
+add_filter( 'display_post_states', 'wbfp_show_branch_info_in_list' );
 
 /*
 Copy taxonomies
 Return taxonomies object that copied post/page
 */
-function copy_post_taxonomies( $original_id, $target_id, $post_type ) {
+function wbfp_copy_post_taxonomies( $original_id, $target_id, $post_type ) {
 	if ( isset( $original_id, $target_id, $post_type ) ) {
 		$taxonomies = get_object_taxonomies( $post_type );
 		$post_terms = wp_get_object_terms( $original_id, $taxonomies );
@@ -165,7 +142,7 @@ function copy_post_taxonomies( $original_id, $target_id, $post_type ) {
 /*
 Copy postmeta
 */
-function copy_post_post_meta( $original_id, $target_id ) {
+function wbfp_copy_post_post_meta( $original_id, $target_id ) {
 	if ( isset( $original_id, $target_id ) ) {
 		$custom_fields = get_post_custom( $original_id );
 		unset(	$custom_fields['_original_post_id'], 
@@ -179,7 +156,7 @@ function copy_post_post_meta( $original_id, $target_id ) {
 		foreach ( $custom_fields as $key => $values ) {
 			foreach ( $values as $value ) {
 				$value = maybe_unserialize( $value );
-				$value = post_meta_addslashes( $value );
+				$value = wbfp_post_meta_addslashes( $value );
 				update_post_meta( $target_id, $key, $value );
 			}
 		}
@@ -189,9 +166,9 @@ function copy_post_post_meta( $original_id, $target_id ) {
 /*
 Postmeta values addslashes
 */
-function post_meta_addslashes( $value ) {
+function wbfp_post_meta_addslashes( $value ) {
 	if (function_exists('map_deep')){
-		return map_deep( $value, 'map_deep_post_meta_addslashes' );
+		return map_deep( $value, 'wbfp_map_deep_post_meta_addslashes' );
 	} else {
 		return wp_slash( $value );
 	}
@@ -200,7 +177,7 @@ function post_meta_addslashes( $value ) {
 /*
 Map_deep hook function for addslashes to value
 */
-function map_deep_post_meta_addslashes( $value ) {
+function wbfp_map_deep_post_meta_addslashes( $value ) {
 	return is_string( $value ) ? addslashes( $value ) : $value;
 }
 
@@ -208,7 +185,7 @@ function map_deep_post_meta_addslashes( $value ) {
 Origin inherited branch's revision
 Change branch's revision postparent to origin
 */
-function inherited_branch_revision( $original_id, $target_id ) {
+function wbfp_inherited_branch_revision( $original_id, $target_id ) {
 	if ( isset( $original_id, $target_id ) ) {
 		if ( $revisions = wp_get_post_revisions( $original_id ) ) {
 			global $wpdb;
@@ -238,7 +215,7 @@ function inherited_branch_revision( $original_id, $target_id ) {
 Origin inherited branch's attachments
 Change branch's attachments postparent to origin
 */
-function inherited_branch_attachments( $original_id, $target_id ) {
+function wbfp_inherited_branch_attachments( $original_id, $target_id ) {
 	$args = array(	'post_type' => 'attachment', 
 					'numberposts' => -1, 
 					'post_status' => null, 
